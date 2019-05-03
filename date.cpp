1cb2d4f1bc3131f5a55840e92a04492eede71737
tiagodusilva <48838198+tiagodusilva@users.noreply.github.com>
2019-05-04 00:15:58 +0100

    Completed Date class

--- date.cpp
+++ date.cpp
@@ -1,4 +1,5 @@
 #include "date.h"
+#include "customUtilities.h"
 #include <iostream>
 #include <iomanip>
 
@@ -18,6 +19,99 @@ Date::Date(unsigned int aYear, unsigned int aMonth, unsigned int aDay)
     day = aDay;
 }
 
+Date::Date(const string& s)
+{
+	int start = 0, end = s.find('/');
+	string sub;
+	if (end != string::npos)
+	{
+		sub = s.substr(0, end);
+		cu::strTrim(sub);
+		this->year = stoi(s);
+
+		start = end + 1;
+		end = s.find('/', start);
+		if (end != string::npos)
+		{
+			sub = s.substr(start, end - start);
+			cu::strTrim(sub);
+			this->month = stoi(sub);
+
+			sub = s.substr(end + 1, string::npos);
+			cu::strTrim(sub);
+			this->day = stoi(sub);
+
+			return;
+		}
+	}
+
+	// Only executes if a verification has failed above
+	this->year = 0;
+	this->month = 0;
+	this->day = 0;
+}
+
+Date Date::now()
+{
+
+		// God Bless Marantes
+
+		/* current date/time based on current system */
+
+		/* LocalTime for MinGW compiler */
+		//time_t now = time(0);
+		//tm* ltm = localtime(&now);
+
+		/* LocalTime for Microsoft Visual Studio compiler */
+		time_t now = time(0);
+		tm* ltm = new tm();
+		localtime_s(ltm, &now);
+
+		Date currentDate = Date( 1900 + ltm->tm_year, 1 + ltm->tm_mon, ltm->tm_mday );
+		return currentDate;
+}
+
+bool Date::readUserInput()
+{
+	int y, m, d;
+	bool fail = false;
+
+	do
+	{
+		if (fail)
+			cout << "Invalid Date!" << endl;
+		if (!cu::readInt(y, "Year"))
+			return false;		
+		if (!cu::readInt(m, "Month"))
+			return false;		
+		if (!cu::readInt(d, "Day"))
+			return false;
+
+		*this = Date(y, m, d);
+		fail = !this->isValid();
+	} while (fail);
+
+	return true;
+
+}
+
+bool Date::readFromFile(ifstream& fin, unsigned int& lineTracker)
+{
+	string s;
+	getline(fin, s);
+	if (fin.eof()) return false;
+
+	*this = Date(s);
+
+	if (!this->isValid())
+		return false;
+	
+	lineTracker++;
+	return true;
+}
+
+#pragma region GETTERS
+
 unsigned int Date::getDay() const
 {
     return day;
@@ -33,12 +127,15 @@ unsigned int Date::getYear() const
     return year;
 }
 
+#pragma endregion
+
 ostream& operator<< (ostream& stream, const Date& date)
 {
-    stream << date.getYear() << "/"
-            << setw(2) << setfill('0')
-            << date.getMonth() << "/"
-            << date.getDay();
+    stream << setfill('0') <<
+			setw(4) << date.getYear() << "/" <<
+			setw(2) << date.getMonth() << "/" <<
+			setw(2) << date.getDay() <<
+			setfill(' ');
     return stream;
 }
 
@@ -55,7 +152,7 @@ bool Date::isLeapYear() const // Evaluate if a given year is a leap year
 
 bool Date::isValid() const
 {
-	const int daysInMonth[13] = { -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	const unsigned int daysInMonth[13] = { -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	if (this->month >= 1 && this->month <= 12)
 	{
 		if (this->month == 2 && this->isLeapYear())
@@ -67,7 +164,7 @@ bool Date::isValid() const
 		}
 		else
 		{
-			if (this->day >= 1 && this->day <= daysInMonth[this->month])
+			if (this->day >= (unsigned) 1 && this->day <= daysInMonth[this->month])
 				return true;
 			else
 				return false;
@@ -76,6 +173,8 @@ bool Date::isValid() const
 	else
 		return false;
 }
+
+#pragma region OPERATOR OVERRIDES
 
 bool Date::operator<(const Date& d) const
 {
@@ -91,17 +190,17 @@ bool Date::operator<(const Date& d) const
 	// Both months are the same
 	if (this->day >= d.getDay())
 		return false;
-    return true; // if (this->day < d.getDay())
+	return true; // if (this->day < d.getDay())
 }
 
-bool Date::operator>=(const Date& d) const
+bool Date::operator>=(const Date & d) const
 {
-    return !(*this < d);
+	return !(*this < d);
 }
 
-bool Date::operator>(const Date& d) const
+bool Date::operator>(const Date & d) const
 {
-    if (this->year > d.getYear())
+	if (this->year > d.getYear())
 		return true;
 	if (this->year < d.getYear())
 		return false;
@@ -113,22 +212,24 @@ bool Date::operator>(const Date& d) const
 	// Both months are the same
 	if (this->day > d.getDay())
 		return true;
-    return false; // if (this->day <= d.getDay())
+	return false; // if (this->day <= d.getDay())
 }
 
-bool Date::operator<=(const Date& d) const
+bool Date::operator<=(const Date & d) const
 {
-    return !(*this > d);
+	return !(*this > d);
 }
 
-bool Date::operator==(const Date& d) const
+bool Date::operator==(const Date & d) const
 {
-    if (this->year != d.getYear() || this->month != d.getMonth() || this->day != d.getDay())
+	if (this->year != d.getYear() || this->month != d.getMonth() || this->day != d.getDay())
 		return false;
-    return true;
+	return true;
 }
 
-bool Date::operator!=(const Date& d) const
+bool Date::operator!=(const Date & d) const
 {
-    return !(*this == d);
+	return !(*this == d);
 }
+
+#pragma endregion
--- date.h
+++ date.h
@@ -8,12 +8,40 @@ class Date
 {
 public:
 	Date();
+	/**
+		WARNING: Does not check if crated Date is valid!
+	*/
 	Date(unsigned int aYear, unsigned int aMonth, unsigned int aDay);
+	/**
+		If the string is in a wrong format, it will set Date to an invalid state
+		(y = 0, m = 0, d = 0)
+		WARNING: Does not check if crated Date is valid!
+	*/
+	Date(const std::string& s);
+	
+	/**
+		Returns a Date object with the current system date
+		May only work in visual studio, requires additional testing
+	*/
+	Date now();
+
+	bool readUserInput();
+	bool readFromFile(std::ifstream& fin, unsigned int& lineTracker);
+
 	unsigned int getDay() const;
 	unsigned int getMonth() const;
 	unsigned int getYear() const;
+
+	/**
+		Returns true if it's a leap year
+	*/
 	bool isLeapYear() const;
+	/*
+		Returns true if the Date object is valid
+		Checks days in months, month belonging [1, 12] and leap yearss 
+	*/
 	bool isValid() const;
+
 	bool operator< (const Date& d) const;
 	bool operator>= (const Date& d) const;
 	bool operator> (const Date& d) const;
